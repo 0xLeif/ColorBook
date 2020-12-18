bd11802cbeeb4b40ebfb09191a36caf67978a8c5
0xLeif <zmeriksen@icloud.com>
2020-12-17 19:03:52 -0600

    Initial Commit

---
+++ ColorBook/ColorBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorBookApp.swift
+//  ColorBook
+//
+//  Created by Zach Eriksen on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct ColorBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
